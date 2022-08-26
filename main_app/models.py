53db6fc1a12038356cf18e9d5fa8e5b1f7f4aa37
thummel1867 <thummel1867@gmail.com>
2022-08-26 13:06:03 -0400

    adds auth functionality for update/delete

--- main_app/models.py
+++ main_app/models.py
@@ -2,14 +2,12 @@ from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
 
-
-from django.contrib.auth.mixins import LoginRequiredMixin
 class Bird(models.Model):
     title = models.CharField(max_length=200)
     scientific_name = models.CharField(max_length=200)
     conservation_status = models.TextField(max_length=1000, blank=True)
     date = models.DateField(blank=True, null=True)
-    image = models.CharField(max_length=500)
+    image = models.CharField(max_length=2000, blank=True)
     family = models.CharField(max_length = 200, blank=True)
     description = models.TextField(blank = True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -20,5 +18,5 @@ class Bird(models.Model):
     def get_absolute_url(self):
         return reverse('detail', kwargs={'bird_id': self.id})
 
-    class Meta:
-        ordering = ['-date']
+    # class Meta:
+    #     ordering = ['-date']
