aca2695c241a95c188b5d919c55187e2fe0157ef
thummel1867 <thummel1867@gmail.com>
2024-04-29 11:03:47 -0400

    updating database

--- news_app/settings.py
+++ news_app/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'news_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bird_app',
+        'NAME': 'news_app',
     }
 }
 
